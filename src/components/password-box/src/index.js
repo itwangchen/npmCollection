db65737d836b5dfb4dc0e7e92364403d519d3629
wangchen <wangchen6@126.com>
2020-08-03 17:50:23 +0800

    更改密码

--- src/components/password-box/src/index.js
+++ src/components/password-box/src/index.js
@@ -1,15 +1,15 @@
 // index.js
-// import wcPassword from './wc-password.vue'
-// export default wcPassword
-
 import wcPassword from './wc-password.vue'
-const install = Vue => {
-  Vue.component('wcPassword', wcPassword)
-}
+export default wcPassword
 
-if (typeof window !== undefined && window.Vue) {
-  install(window.Vue)
-}
+// import wcPassword from './wc-password.vue'
+// const install = Vue => {
+//   Vue.component('wcPassword', wcPassword)
+// }
 
-export default { install }
+// if (typeof window !== undefined && window.Vue) {
+//   install(window.Vue)
+// }
+
+// export default { install }
 
